94226ef943e5c9b0907d24ee1a781745fc16461c
Yurii <yura.vashchenko@gmail.com>
2017-02-23 19:19:06 +0200

    add

--- application/views/outside/pages/yura_vashchenko_auth/login.php
+++ application/views/outside/pages/yura_vashchenko_auth/login.php
@@ -1,11 +1,11 @@
 <form class="form-horizontal login-form" method="post">
     <div class="form-group">
-        <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Email"
+        <input type="email" name="email" class="form-control" id="email" placeholder="Email"
                value="<?= set_value('email'); ?>" data-toggle="tooltip" data-placement="left" title="Type your email"
                required>
     </div>
     <div class="form-group">
-        <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password"
+        <input type="password" name="password" class="form-control" id="password" placeholder="Password"
                data-toggle="tooltip" data-placement="left" title="Type your password" required>
     </div>
     <div class="form-group">
